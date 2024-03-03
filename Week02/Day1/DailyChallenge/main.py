a75b6cbcc5aa20d82b7e8d13fce6f07ba7bfcc19
danielheath0 <daniel.heath0@gmail.com>
2024-03-03 13:44:34 +0200

    Week 2 Daily Challenge completed

---
+++ Week02/Day1/DailyChallenge/main.py
@@ -0,0 +1,19 @@
+import random
+
+exercise_string = input("Please feed me a 10-character string!")
+
+if len(exercise_string) < 10:
+    print("Not long enough!")
+elif len(exercise_string) > 10:
+    print("Too long!")
+elif len(exercise_string) == 10:
+    print(exercise_string[0], exercise_string[-1])
+    
+    counter = ""
+    for i in exercise_string:
+        counter = counter + i
+        print(counter)
+        
+newlist = list(counter)
+random.shuffle(newlist)
+print("".join(newlist))
