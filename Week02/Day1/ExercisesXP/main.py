e0d69ec5e1fb9673f5a4c8bfd8ee38af9919163f
danielheath0 <daniel.heath0@gmail.com>
2024-03-03 13:17:08 +0200

    Week 2 Exercise 1 complete

---
+++ Week02/Day1/ExercisesXP/main.py
@@ -0,0 +1,86 @@
+#Exercise 1
+
+print("Hello world \n"*4)
+
+
+
+
+
+#Exercise 2
+
+print((99**3)*8)
+
+
+
+
+
+#Exercise 3
+print("False, True, False, Error, False")
+
+print( 5 < 3)
+print( 3 == 3)
+print( 3 == "3")
+#print( "3" > 3)
+print( "Hello" == "hello")
+
+#Exercise 4
+computer_brand = "Microsoft Surface Laptop 4"
+print(f"I have a {computer_brand} computer")
+
+#Exercise 5
+
+name = "Daniel"
+age = "37"
+shoe_size = "43 1/3"
+info = f"My name is {name}, I'm {age} and my shoe size is {shoe_size}."
+print(info)
+
+
+
+
+#Exercise 6
+
+a = 5
+b = 3
+if a > b:
+    print("Hello World")
+
+
+
+
+
+#Exercise 7
+
+oddoreven = int(input("Please feed me an integer: "))
+if oddoreven % 2 == 0:
+    print("Delicious - I love even numbers!")
+else:
+    print("Bleurgh! Odd numbers are disgusting!")
+
+
+
+
+#Exercise 8
+
+your_name = input("What is your name? ")
+if your_name == "Daniel":
+    print("Wow! We have the same name!")
+else:
+    print("Booooo, you have a stupid name.")    
+
+
+
+
+
+#Exercise 9
+
+height = (int(input("Please tell me your height in inches:")))*2.54
+
+if height >= 145:
+    print("You are tall enough to ride!")
+else:
+    print("Sorry - you need to grow more :(")
+    
+
+
+
