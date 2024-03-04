9aab1889e45515ac131235c9384a9cc7878d6ef2
danielheath0 <daniel.heath0@gmail.com>
2024-03-04 15:31:26 +0200

    week 2 day 2 daily challenge

---
+++ Week02/Day2/DailyChallenge/main.py
@@ -0,0 +1,26 @@
+#Challenge 1
+
+number = int(input("Please enter a number: "))
+length = int(input("Please enter a length: "))
+
+sequence = []
+
+for i in range(1,length + 1):
+    sequence.append(i*number)
+    
+
+print(sequence)  
+
+
+#Challenge 2
+
+userword = input("Please enter a word: ")
+
+result = [""]
+
+for letter in userword:
+    if letter != result[-1]:
+        result.append(letter)
+    
+result.pop(0)    
+print("".join(result))
