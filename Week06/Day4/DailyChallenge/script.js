73cb92885cb729c969d487badc3de15e72e60c95
danielheath0 <daniel.heath0@gmail.com>
2024-04-03 15:32:27 +0300

    Week 6 Day 4 DC

---
+++ Week06/Day4/DailyChallenge/script.js
@@ -0,0 +1,19 @@
+const form = document.getElementById('myForm')
+const outputDiv = document.getElementById('output')
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const firstName = document.getElementById('firstName').value;
+    const lastName = document.getElementById('lastName').value;
+
+    const formData = {
+        firstName: firstName,
+        lastName: lastName
+    }
+
+
+const jsonData = JSON.stringify(formData);
+
+outputDiv.textContent = jsonData;
+}
+);
