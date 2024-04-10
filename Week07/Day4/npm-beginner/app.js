6be6dd8651a63a1cb2537a34395b500e8f7e9a69
danielheath0 <daniel.heath0@gmail.com>
2024-04-10 21:31:52 +0300

    Week 7 Day 4 XP

---
+++ Week07/Day4/npm-beginner/app.js
@@ -0,0 +1,9 @@
+import chalk from 'chalk'
+
+console.log(chalk.red('Hello world!'));
+console.log(chalk.green('Hello world!'));
+console.log(chalk.magenta('Hello world!'));
+console.log(chalk.blue('Hello world!'));
+console.log(chalk.cyan('Hello world!'));
+console.log(chalk.greenBright('Hello world!'));
+console.log(chalk.whiteBright('Hello world!'));
